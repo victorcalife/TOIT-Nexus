6663e887692fafef160c2ba5a4cab99437945738
Victor Calife <victorcalife@gmail.com>
2025-08-05 03:42:18 -0300

    test: App minimal com apenas 4 páginas essenciais - eliminando imports problemáticos

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App-simple";
+import App from "./App-minimal";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
